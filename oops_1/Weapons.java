f2c4bfd96ee1176e63c49d6864abf83ce1619d40
Sanskar2001 <sanskaratrey22@gmail.com>
2023-04-20 15:11:42 +0530

    lec14

---
+++ oops_1/Weapons.java
@@ -0,0 +1,24 @@
+class Gun
+{
+    String gunName;
+    float calibre;
+    int ammo;
+    int damage;
+
+    public Gun(String gunName,float y,int z,int k)
+    {
+        gunName=x;
+        calibre=y;
+        ammo=z;
+        damage=k;
+    }
+
+
+}
+
+class Weapons
+{
+    public static void main(String[] args) {
+        
+    }
+}
