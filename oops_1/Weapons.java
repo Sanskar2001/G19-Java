5920eeefc0b2e77999ef4307ad3f199d696e8a59
Sanskar2001 <sanskaratrey22@gmail.com>
2023-04-21 10:55:41 +0530

    lec14

--- oops_1/Weapons.java
+++ oops_1/Weapons.java
@@ -5,12 +5,20 @@ class Gun
     int ammo;
     int damage;
 
-    public Gun(String gunName,float y,int z,int k)
+    public Gun(String gunName,float calibre,int ammo,int damage)
     {
-        gunName=x;
-        calibre=y;
-        ammo=z;
-        damage=k;
+        this.gunName=gunName;
+        this.calibre=calibre;
+        this.ammo=ammo;
+        this.damage=damage;
+    }
+
+    public void display()
+    {
+        System.out.println(this.gunName);
+        System.out.println(this.calibre);
+        System.out.println(this.ammo);
+        System.out.println(this.damage);
     }
 
 
@@ -20,5 +28,15 @@ class Weapons
 {
     public static void main(String[] args) {
         
+
+        Gun gun=new Gun("AKM", 7.62f, 30, 50);
+
+        // gun.display();
+
+
+        System.out.println(gun.gunName);
+        System.out.println(gun.calibre);
+        System.out.println(gun.ammo);
+        System.out.println(gun.damage);
     }
 }
