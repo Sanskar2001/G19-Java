5f2374ba07e2c34aff80a5a840b08bbfcd3e59b4
Sanskar2001 <sanskaratrey22@gmail.com>
2023-04-21 11:34:37 +0530

    lec14

---
+++ oops_2/Engine.java
@@ -0,0 +1,89 @@
+class Engine
+{
+    private boolean isEngineOn;
+    int noOFPistons;
+    int fuelEfficieny;
+    int maxRpm;
+    int currentRpm;
+
+    public Engine(boolean isEngineOn,int noOFPistons,
+    int fuelEfficieny,int currentRpm,int maxRpm)
+    {
+        this.isEngineOn=isEngineOn;
+        this.noOFPistons=noOFPistons;
+        this.fuelEfficieny=fuelEfficieny;
+        this.currentRpm=currentRpm;
+        this.maxRpm=maxRpm;
+
+        System.out.println("Constructor called Engine Created");
+    }
+
+    // void ignitionOn()
+    // {
+    //     this.isEngineOn=true;
+    // }
+
+    // boolean checkEngineStatus()
+    // {
+    //     return this.isEngineOn;
+    // }
+
+    void accelerate()
+    {
+        this.currentRpm+=200;
+    }
+
+    void setIsEngineOn(boolean isEngineOn)
+    {
+        this.isEngineOn=isEngineOn;
+    }
+
+    boolean getIsEngineOn()
+    {
+        return this.isEngineOn;
+    }
+    
+    void setNoOFPistons(int noOFPistons)
+    {
+        this.noOFPistons=noOFPistons;
+    }
+
+    int getNoOFPistons()
+    {
+        return this.noOFPistons;
+    }
+
+    void setFuelEfficieny(int fuelEfficieny)
+    {
+        this.fuelEfficieny=fuelEfficieny;
+    }
+
+    int getFuelEfficiency()
+    {
+        return this.fuelEfficieny;
+    }
+
+    void setMaxRpm(int maxRpm)
+    {
+        this.maxRpm=maxRpm;
+    }
+
+    int getMaxRpm()
+    {
+        return this.maxRpm;
+    }
+
+    void setCurrentRpm(int currentRpm)
+    {
+        this.currentRpm=currentRpm;
+    }
+
+    int getCurrentRpm()
+    {
+        return this.currentRpm;
+    }
+
+
+
+
+}
---
+++ oops_2/engineClient.java
@@ -0,0 +1,37 @@
+ class engineClient {
+    
+
+    public static void main(String[] args) {
+        
+        Engine engine=new Engine(false, 5, 10, 0, 5000);
+
+        // System.out.println(engine.isEngineOn);
+        // System.out.println(engine.fuelEfficieny);
+        // System.out.println(engine.noOFPistons);
+        // System.out.println(engine.currentRpm);
+        // System.out.println(engine.maxRpm);
+
+
+        // System.out.println(engine.isEngineOn);
+        // engine.isEngineOn=true;
+        // System.out.println(engine.isEngineOn);
+
+        // System.out.println("BEFORE:"+engine.checkEngineStatus());
+        // engine.ignitionOn();
+        // System.out.println("AFTER:"+engine.checkEngineStatus());
+
+
+
+        engine.setIsEngineOn(true);
+        System.out.println(engine.getIsEngineOn());
+        engine.setIsEngineOn(false);
+        System.out.println(engine.getIsEngineOn());
+
+
+        System.out.println(engine.getCurrentRpm());
+        engine.accelerate();
+        System.out.println(engine.getCurrentRpm());
+        engine.accelerate();
+        System.out.println(engine.getCurrentRpm());
+    }
+}
