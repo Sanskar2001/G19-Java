136b2a15141329b42f0623b1b072bd77a8cde7be
Sanskar2001 <sanskaratrey22@gmail.com>
2023-04-24 11:31:49 +0530

    lec16

--- oops_3/Player.java
+++ oops_3/Player.java
@@ -5,6 +5,7 @@ class Player {
     Gun gun;
     boolean helmet; // helmet increases hp by 50
     boolean armour; // armour increases hp by 50
+    boolean isAlive; // to check whether player is active or down
 
 
     public Player(int playerId)
@@ -14,6 +15,7 @@ class Player {
         this.gun= new Gun(Gunname.Pan, 0, 0, 20);
         this.helmet=false;
         this.armour=false;
+        this.isAlive=true;
     }
 
     public Player(int playerId,Gun gun)
@@ -23,6 +25,7 @@ class Player {
         this.health=100;
         this.helmet=false;
         this.armour=false;
+        this.isAlive=true;
     }
 
     public Player(int playerId,Gun gun,boolean helmet,boolean armour)
@@ -31,6 +34,7 @@ class Player {
         this.gun=gun;
         this.armour=armour;
         this.helmet=helmet;
+        this.isAlive=true;
 
        this.health=100;
 
@@ -39,5 +43,18 @@ class Player {
 
        if(helmet)
        this.health+=50;
+    }
+
+    void attack(Player p)
+    {
+        p.health=p.health-this.gun.getDamage();
+        if(p.health<=0)
+        p.isAlive=false;
+    }
+
+    void revive(Player p)
+    {
+        if(p.isAlive==false)
+        p.health=100;
     }
 }
