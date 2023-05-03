da6ee4926a4cbb7e2e3a3e27c0363bfe6c1f7c2c
Sanskar2001 <sanskaratrey22@gmail.com>
2023-05-03 16:11:56 +0530

    lec17

--- Strings/Substrings.java
+++ Strings/Substrings.java
@@ -15,13 +15,28 @@ class Substrings {
 
     }
 
+    static void printAllSubstringsReverse(String str)
+    {
+
+        int n=str.length();
+        for(int st=n-1;st>=0;st--)
+        {
+            for(int en=st+1;en<=n;en++)
+            {
+                System.out.println(str.substring(st, en));
+            }
+        }
+
+    }
+
 
     public static void main(String[] args) {
 
         String str="abc";
 
-        printAllSubstrings(str);
+        // printAllSubstrings(str);
         
+        printAllSubstringsReverse(str);
 
         
     }
