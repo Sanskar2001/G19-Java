62cda6e79a4053f6c3041d124eb227c0dfcc9c90
Sanskar2001 <sanskaratrey22@gmail.com>
2023-05-15 16:27:17 +0530

    lec25

--- Queue/CircularQueue.java
+++ Queue/CircularQueue.java
@@ -1,22 +1,15 @@
 class CircularQueue {
     
-    int []arr;
+    int [] arr;
     int front;
     int size;
+
 
     public CircularQueue(int capacity)
     {
         arr=new int[capacity];
         front=0;
         size=0;
-    }
-
-    boolean isFull()
-    {
-        if(size==arr.length)
-        return true;
-
-        return false;
     }
 
     boolean isEmpty()
@@ -27,44 +20,41 @@ class CircularQueue {
         return false;
     }
 
+    boolean isFull()
+    {
+        if(size==arr.length)
+        return true;
+
+        return false;
+    }
+
     void enQueue(int ele) throws Exception
     {
-
         if(isFull())
-        throw new Exception("Queue is Full");
+        throw new Exception("Queue is Full!! Can't enQueue any element");
 
 
-        int index=(front+size)%arr.length;
-        arr[index]=ele;
+        int idx=(front+size)%arr.length;
+        arr[idx]=ele;
         size++;
     }
 
-    int deQueue() throws Exception 
+    int deQueue() throws Exception
     {
         if(isEmpty())
-        throw new Exception("Queue is Empty!!");
+        throw new Exception("Queue is already Empty!!");
+
 
         int deletedElement=arr[front];
         front++;
-        size--;
 
         if(front==arr.length)
         front=0;
 
         return deletedElement;
-
     }
 
-    void display() throws Exception
-    {
-        if(isEmpty())
-        throw new Exception("Queue is Empty");
+    
 
-        for(int i=0;i<size;i++)
-        {
-            int idx=(front+i)%arr.length;
 
-            System.out.println(arr[idx]);
-        }
-    }
 }
