4f41c9ab6593effb5757b73659d1fcdbcb568cd8
Sanskar2001 <sanskaratrey22@gmail.com>
2023-05-22 11:36:54 +0530

    lec29

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -14,5 +14,51 @@ class LinkedList
 {
     Node head;
 
-    
+    void insertionAtHead(int data)
+    {
+        if(head==null)
+        {
+            head=new Node(data);
+            return;
+        }
+
+        Node newNode=new Node(data);
+        newNode.next=head;
+        head=newNode;
+    }
+
+    void deleteAtHead()
+    {
+        Node tmp=head;
+        head=head.next;
+        tmp.next=null;
+    }
+
+
+    void insertionAtKthPosition(int data,int k)
+    {
+
+        if(k==0)
+        {
+            insertionAtHead(data);
+            return;
+           
+        }
+
+        Node tmp=head;
+
+        for(int i=1;i<=(k-1);i++)
+        tmp=tmp.next;
+
+
+        Node newNode=new Node(data);
+        newNode.next=tmp.next;
+        tmp.next=newNode;
+    }
+
+    void deletionAtKthPosition()
+    {
+        
+    }
+
 }
