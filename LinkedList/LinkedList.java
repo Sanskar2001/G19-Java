9c79e9fe0ea48081bebfe4acacc46b99e2bf8b4d
Sanskar2001 <sanskaratrey22@gmail.com>
2023-05-22 10:56:09 +0530

    lec29

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -2,46 +2,17 @@ class Node
 {
     int data;
     Node next;
-
+    
     public Node(int data)
     {
         this.data=data;
-        next=null;
+        this.next=null;
     }
 }
 
 class LinkedList
 {
     Node head;
-
-
-    void insertionAtHead(int data)
-    {
-
-        Node newNode=new Node(data);
-        newNode.next=head;
-        head=newNode;
-    }
-
-    void deletionAtHead()
-    {
-        Node tmp=head;
-        head=head.next;
-        // disconnecting old head i.e. tmp from LL
-        tmp.next=null;
-
-    }
-    void display()
-    {
-        Node tmp=head;
-
-        while (tmp!=null) {
-            
-            System.out.print(tmp.data+" ");
-            tmp=tmp.next;
-        }
-
-    }
 
     
 }
