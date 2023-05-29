d6c24e83c7d767e56cd34f9dc5b2252ed6518182
Sanskar2001 <sanskaratrey22@gmail.com>
2023-05-29 11:35:42 +0530

    lec34

--- IOStream/FileCopy.java
+++ IOStream/FileCopy.java
@@ -1,8 +1,29 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 class FileCopy {
     
     public static void main(String[] args) {
-        
+         
+        try {
+            FileInputStream fis=new FileInputStream("/Users/sanskaratrey/Documents/G19-Java/IOStream/f1.txt");
 
-        
+            byte[] arr=new byte[fis.available()];
+            fis.read(arr);
+
+            FileOutputStream fos=new FileOutputStream("/Users/sanskaratrey/Documents/G19-Java/IOStream/f2.txt");
+            fos.write(arr);
+
+
+            fis.close();
+            fos.close();
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+
     }
 }
