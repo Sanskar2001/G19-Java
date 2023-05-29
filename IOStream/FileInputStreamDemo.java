c6b7ab12405b65babc208fb57fa39138645aa04a
Sanskar2001 <sanskaratrey22@gmail.com>
2023-05-29 11:08:56 +0530

    lec34

---
+++ IOStream/FileInputStreamDemo.java
@@ -0,0 +1,34 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+class FileInputStreamDemo
+{
+    public static void main(String[] args) {
+        
+        try {
+            FileInputStream fis=new FileInputStream("/Users/sanskaratrey/Documents/G19-Java/IOStream/file.txt");
+            // System.out.println((char)fis.read());
+
+
+            // int x=fis.read();
+
+            // while(x!=-1)
+            // {
+            //     System.out.print((char)x);
+            //     x=fis.read();
+            // }
+
+
+            byte[] arr=new byte[fis.available()];
+            fis.read(arr);
+            String str=new String(arr);
+            System.out.println(str);
+
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+}
